5aae6e518660d31c1c40e2289adb0b0968a011f0
Leogy <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-11-29 16:57:41 +0800

    added docker ffile

--- obj/Debug/net10.0/EcommerceBackend.AssemblyInfo.cs
+++ obj/Debug/net10.0/EcommerceBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2105deda3370956ebbfd9d32794ddc871486fbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ac104097b0d983dbeaa2838171e56ed66d73a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
