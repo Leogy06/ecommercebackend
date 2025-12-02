3bac10380ab98cec8145dc7c620074c8fc7281e7
Leogy <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-12-02 10:29:13 +0800

    idkj some changes

--- obj/Debug/net10.0/EcommerceBackend.AssemblyInfo.cs
+++ obj/Debug/net10.0/EcommerceBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88728657574747eebcd2119124261dbba848dbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e31bc51eaec97bb6f9052c586b66fc675f87231")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
